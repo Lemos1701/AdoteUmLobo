f00a289f1cc16a862ad743a4fe02bf7dec9e0248
Ayrton Surica <ayrtonsurica@gmail.com>
2024-07-30 17:10:01 -0300

    feat(show-lobinho): usando local storage para pegar itens desejados

---
+++ scripts/showLobinhoScript.js
@@ -0,0 +1,4 @@
+document.getElementById("imagem").src = localStorage.getItem("imagem")
+document.getElementById("nome").innerHTML = localStorage.getItem("nome")
+document.getElementById("texto").innerHTML = localStorage.getItem("descricao")
+
