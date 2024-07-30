14b3c106fa300789c1e03febf33e31d96f9ccce2
Paulo Jorge Campos Cardoso <pjcardoso25@gmail.com>
2024-07-30 15:30:00 -0300

    feat: add script addLobo

---
+++ scripts/addLobo.js
@@ -0,0 +1,36 @@
+const objeto = lobinhos
+let btnSave = document.querySelector(".btnSave")
+let nome = document.querySelector("#inputName").value
+console.log(nome)
+
+function addlobo(){
+    let nome = document.querySelector("#inputName").value
+    let idade = document.querySelector("#inputYear").value
+    let link = document.querySelector("#inputLink").value
+    let descricao = document.querySelector("#textareaDescription").value
+    let newObj = {
+        "id": objeto.length + 1,
+        "nome": nome,
+        "idade": idade,
+        "descricao": descricao,
+        "imagem":link,
+        "adotado" : false
+    }
+    objeto.push(newObj)
+}
+
+btnSave.addEventListener("click",()=>{
+    let nome = document.querySelector("#inputName").value
+    let idade = document.querySelector("#inputYear").value
+    let link = document.querySelector("#inputLink").value
+    let descricao = document.querySelector("#textareaDescription").value
+    if(nome == "" || idade == "" || link == "" || descricao == ""){
+        alert("você esqueceu de preencher algum campo");
+    }else{
+        addlobo()
+        inputName.value = "";
+        inputYear.value = "";
+        inputLink.value = "";
+        textareaDescription.value = ""
+    }
+})
