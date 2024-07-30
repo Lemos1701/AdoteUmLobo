49a575a3ef25e421f258f2ff3029211aaac07af1
Ayrton Surica <ayrtonsurica@gmail.com>
2024-07-30 17:28:32 -0300

    feat(show-lobinho): incluindo funcionalidade para pegar id em add lobo

--- scripts/showLobinhoScript.js
+++ scripts/showLobinhoScript.js
@@ -1,4 +1,10 @@
 document.getElementById("imagem").src = localStorage.getItem("imagem")
 document.getElementById("nome").innerHTML = localStorage.getItem("nome")
 document.getElementById("texto").innerHTML = localStorage.getItem("descricao")
+let idNum = localStorage.getItem("number")
+console.log(localStorage.getItem("id"+idNum))
 
+document.getElementById("adote").addEventListener("click", function() {
+    localStorage.setItem("id", localStorage.getItem("id"+idNum))
+    window.location.href = "../html/adoteLobo.html"
+});
