e8d3489e517e7a8c544b8d31e56de0db50747056
Ayrton Surica <ayrtonsurica@gmail.com>
2024-07-30 19:26:00 -0300

    feat(add-lobo): funcionalidade de adicionar lobo

--- scripts/addLobo.js
+++ scripts/addLobo.js
@@ -1,3 +1,9 @@
+let listaLobos = localStorage.getItem("lobinhos")
+console.log(listaLobos)
+if (listaLobos) {
+    var lobinhos = JSON.parse(listaLobos);
+    console.log(lobinhos);
+}
 const objeto = lobinhos;
 let btnSave = document.querySelector(".btnSave");
 console.log(objeto);
@@ -7,15 +13,21 @@ function addlobo(){
     let idade = document.querySelector("#inputYear").value;
     let link = document.querySelector("#inputLink").value;
     let descricao = document.querySelector("#textareaDescription").value;
+    let id = localStorage.getItem("idMin")
+    console.log(id)
     let newObj = {
-        "id": objeto.length + 1,
+        "id": parseInt(id) + 1,
         "nome": nome,
         "idade": idade,
         "descricao": descricao,
         "imagem":link,
         "adotado" : false
     };
-    objeto.push(newObj);
+    console.log(parseInt(id) + 1)
+    localStorage.setItem("idMin", parseInt(id)+1)
+    objeto.unshift(newObj);
+    const objetoComoString = JSON.stringify(objeto);
+    localStorage.setItem("lobinhos", objetoComoString)
 }
 
 btnSave.addEventListener("click",()=>{
